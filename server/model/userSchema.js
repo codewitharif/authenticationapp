79b0a6a220fbea8d5b988663f44e5dbd2b073a94
Md Arif <aatif149209@gmail.com>
2024-02-19 01:07:34 +0530

    handling cors after long time and updated auth.js

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-// const dotenv = require("dotenv");
 
-// dotenv.config({ path: "../config.env" });
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
