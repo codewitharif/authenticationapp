58455cb4e5480614ec15c3a20868c634195c27e2
Md Arif <aatif149209@gmail.com>
2024-02-19 16:47:38 +0530

    handlling cookie creation

--- server/router/auth.js
+++ server/router/auth.js
@@ -54,7 +54,6 @@ router.post("/login", async (req, res) => {
           expires: new Date(Date.now() + 25892000000),
           httpOnly: true,
           secure: true,
-          sameSite: "Strict",
         });
         const result = {
           userLogin,
